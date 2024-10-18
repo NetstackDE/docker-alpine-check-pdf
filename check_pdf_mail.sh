414e2aa702ffdd65fa14d53af8dce4b0e2eb351c
Andreas Pfeiffer <pfeiffer@netstack.de>
2024-10-18 23:57:37 +0200

    change mailsend to mailgun API

--- check_pdf_mail.sh
+++ check_pdf_mail.sh
@@ -5,56 +5,29 @@ stammverzeichnis="/data/"
 email_empfaenger="${EMAIL_EMPFAENGER}"
 
 # SMTP-Variablen aus der Umgebung laden
-smtp_server="${SMTP_SERVER}"
-smtp_port="${SMTP_PORT}"
-smtp_user="${SMTP_USER}"
-smtp_auth="on"
-smtp_passwort="${SMTP_PASSWORT}"
-config_file="/etc/msmtprc"
-
-# Funktion zum Erstellen der msmtp-Konfigurationsdatei
-create_msmtp_config() {
-  cat > "$config_file" << EOF
-account studiomitte
-# SMTP-Server und Port
-host $smtp_server
-port $smtp_port
-
-# Authentifizierung
-auth $smtp_auth
-user $smtp_user
-password $smtp_password
-
-# TLS
-tls on
-EOF
-}
-
-# Überprüfen, ob die Datei bereits existiert und warnen
-if [ -f "$config_file" ]; then
-  echo "Warnung: Die Datei '$config_file' existiert bereits. Sie wird überschrieben."
-fi
-
-# Konfigurationsdatei erstellen
-create_msmtp_config
-
-echo "msmtp-Konfigurationsdatei '$config_file' erfolgreich erstellt."
+mailgun_apikey="${MAILGUN_APIKEY}"
+mailgun_domain="${MAILGUN_DOMAIN}"
+mailgun_tag="${MAILGUN_TAG}"
+mail_from="${MAIL_FROM}"
 
 # Aktuelles Datum ermitteln
 aktuelles_datum=$(date +%d-%m-%Y)
 
 # Funktion zum Senden einer E-Mail
 function sende_email() {
-  body="Neue PDF-Datei erstellt im Verzeichnis $verzeichnis";
-  subject="Kopierreport $aktuelles_datum $pdf_datei";
+  anhang=$1  # Accept the first argument as the attachment file path
+  dir=$2 # Folder
+  body="Neue PDF-Datei erstellt im Verzeichnis $dir"
+  subject="Kopierreport $aktuelles_datum $anhang"
+  
   # E-Mail versenden
-  msmtp -v -t << EOF
-Subject: $subject
-From: $smtp_user
-To: $email_empfaenger
-
-$body
-EOF
+  curl -s --user "api:$mailgun_apikey" \
+    "https://api.eu.mailgun.net/v3/$mailgun_domain/messages" \
+    -F from="StudioMitte Kopierreport - <$mail_from>" \
+    -F subject="$subject" \
+    -F to="$email_empfaenger" \
+    -F attachment="@$anhang" \
+    -F o:tag="$mailgun_tag"
 }
 
 # Funktion zum Prüfen auf neue Dateien
@@ -65,7 +38,8 @@ function pruefe_dateien() {
     echo "start file search";
     for pdf_datei in $(find "$verzeichnis" -name "*.pdf"); do
       echo "find PDF file";
-      sende_email
+      pdf_file_fullpath="$verzeichnis/$pdf_datei"
+      sende_email "$pdf_file_fullpath" "$verzeichnis"
       break  # Abbruch, wenn eine neue PDF gefunden wurde
     done
   done
